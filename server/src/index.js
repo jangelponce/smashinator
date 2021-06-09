9533bebafd774e5277a2ced2cba554f47db9feee
Angel Ponce <j.angelponce.esc@gmail.com>
2021-06-09 15:54:12 -0600

    Adding server container

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use(require('./routes/index'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+})
